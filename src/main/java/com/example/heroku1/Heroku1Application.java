f693c039e3ebe2f928cc21d347d0f0697b6a1bf1
daryl <darkare@gmail.com>
2018-04-09 15:36:57 +1000

    make it better

---
+++ src/main/java/com/example/heroku1/Heroku1Application.java
@@ -0,0 +1,22 @@
+package com.example.heroku1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Heroku1Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World 1";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Heroku1Application.class, args);
+	}
+}
